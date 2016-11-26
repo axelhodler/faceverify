dfcb45742bf121eec2c4dbeef428bec594419666
Axel Hodler <derxor@gmail.com>
2016-11-26 13:05:18 +0100

    improve structure

--- sample.py
+++ sample.py
@@ -13,9 +13,13 @@ img_url = 'https://avatars2.githubusercontent.com/u/1504952?v=3&s=460'
 face1 = fetch_faceid(img_url)
 
 IMG_ON_FS='userimage.png'
+
+def write_to_image(text):
+  with open(IMG_ON_FS, "wb") as fh:
+    fh.write(base64.decodestring(text))
+
 file = open('base64imgtext', 'r')
-with open(IMG_ON_FS, "wb") as fh:
-  fh.write(base64.decodestring(file.read()))
+write_to_image(file.read())
 
 result = CF.face.verify(face1, fetch_faceid(IMG_ON_FS))
 print result
