b4af5c1203cb42238804d2140db164229d375673
Axel Hodler <derxor@gmail.com>
2016-11-26 19:28:05 +0100

    can refund payment

--- server/postbank.py
+++ server/postbank.py
@@ -5,38 +5,52 @@ import json
 API_KEY=os.environ['POSTBANK_API_KEY']
 USERNAME=os.environ['POSTBANK_USERNAME']
 PASSWORD=os.environ['POSTBANK_PASSWORD']
+USERNAME_ORGA=os.environ['POSTBANK_ORGA_USERNAME']
 
-def get_token():
+def get_token(username):
   headers= {
           "API-Key": API_KEY
           }
   data={
-    'username': USERNAME,
+    'username': username,
     'password': PASSWORD
   }
   r = requests.post('https://hackathon.postbank.de/bank-api/gold/postbankid/token', headers=headers, data=data)
   return json.loads(r.text)['token']
 
-def finalize_deposit_payment(finalization_url):
+def extract_finalize_url(response):
+  return json.loads(response)['links'][0]['href']
+
+def finalize_deposit_payment(finalization_url, username):
   headers= {
     "API-Key": API_KEY,
-    "x-auth": get_token(),
+    "x-auth": get_token(username),
     "Content-Type":"application/json"
   }
   r = requests.get(finalization_url, headers=headers)
-  print r.text
 
 # Mario (Attendee) to Mariu (Event Organizer)
 def deposit_payment():
   headers= {
     "API-Key": API_KEY,
-    "x-auth": get_token(),
+    "x-auth": get_token(USERNAME),
     "Content-Type":"application/json"
   }
   data='{"authorizationDevice": {"identifier": "668357","method": "bestSign","numberOfFailedAttempts": 0,"authorizationState": "SELECTED", "preferred": false, "aliasName": "HackathonNov01_8" }, "state": null, "creditTransfer": { "amount": 1, "recipient": { "iban": "DE18100100100005616140", "paymentName": "Mariu Freudeafdd", "accountHolder": "Mariu Freudeafdd" }, "sender": { "iban": "DE44100100100005571149", "paymentName": "Mario Liebeafdk", "accountHolder": "Mario Liebeafd" }, "bookingDate": 1480175147919 }}'
   r = requests.post('https://hackathon.postbank.de:443/bank-api/gold/postbankid/credittransfer', headers=headers, data=data)
-  finalizing_url = json.loads(r.text)['links'][0]['href']
-  finalize_deposit_payment(finalizing_url)
+  finalizing_url = extract_finalize_url(r.text)
+  finalize_deposit_payment(finalizing_url, USERNAME)
 
+# Mariu (Event Organizer) gives Mario (Attendee) his money back
+def refund_deposit():
+  headers= {
+    "API-Key": API_KEY,
+    "x-auth": get_token(USERNAME_ORGA),
+    "Content-Type":"application/json"
+  }
+  data='{"authorizationDevice": {"identifier": "668416","method": "bestSign","numberOfFailedAttempts": 0,"authorizationState": "SELECTED","preferred": false,"aliasName": "HackathonNov03_1"},"state": null,"creditTransfer": {"amount": 1,"recipient": {"iban": "DE44100100100005571149","paymentName": "Mario Liebeafdk","accountHolder": "Mario Liebeafdk"},"sender": {"iban": "DE18100100100005616140","paymentName": "Mariu Freudeafdd","accountHolder": "Mariu Freudeafdd"},"bookingDate": 1480175147919}}'
 
+  r = requests.post('https://hackathon.postbank.de:443/bank-api/gold/postbankid/credittransfer', headers=headers, data=data)
+  finalizing_url = extract_finalize_url(r.text)
+  finalize_deposit_payment(finalizing_url, USERNAME_ORGA)
 
