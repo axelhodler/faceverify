ff213253a1ada0eeb24ce7fa31529a1b39becc7d
Axel Hodler <derxor@gmail.com>
2016-11-26 12:56:29 +0100

    sample use img on fs to verify

--- sample.py
+++ sample.py
@@ -1,4 +1,5 @@
 import cognitive_face as CF
+import base64
 
 KEY = '0f4b97a384544170ac4b30161f48daa1'  # Replace with a valid Subscription Key here.
 CF.Key.set(KEY)
@@ -10,9 +11,11 @@ def fetch_faceid(image_url):
 img_url = 'https://avatars2.githubusercontent.com/u/1504952?v=3&s=460'
 face1 = fetch_faceid(img_url)
 
-img_url = 'https://a248.e.akamai.net/secure.meetupstatic.com/photos/member/4/8/d/c/member_230958652.jpeg'
-face2 = fetch_faceid(img_url)
+IMG_ON_FS='userimage.png'
+file = open('base64imgtext', 'r')
+with open(IMG_ON_FS, "wb") as fh:
+  fh.write(base64.decodestring(file.read()))
 
-result = CF.face.verify(face1, face2)
+result = CF.face.verify(face1, fetch_faceid(IMG_ON_FS))
 print result
 
