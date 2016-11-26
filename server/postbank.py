490c87e90a7cdb8b943471297d0479f3152d9531
Axel Hodler <derxor@gmail.com>
2016-11-26 18:56:18 +0100

    hardcode deposit payment

--- server/postbank.py
+++ server/postbank.py
@@ -17,4 +17,14 @@ def get_token():
   r = requests.post('https://hackathon.postbank.de/bank-api/gold/postbankid/token', headers=headers, data=data)
   return json.loads(r.text)['token']
 
+# Mario to Mariu
+def deposit_payment():
+  headers= {
+    "API-Key": API_KEY,
+    "x-auth": get_token(),
+    "Content-Type":"application/json"
+  }
+  data='{"authorizationDevice": {       "identifier": "668357","method": "bestSign","numberOfFailedAttempts": 0,"authorizationState": "SELECTED", "preferred": false, "aliasName": "HackathonNov01_8" }, "state": null, "creditTransfer": { "amount": 1, "recipient": { "iban": "DE18100100100005616140", "paymentName": "Mariu Freudeafdd", "accountHolder": "Mariu Freudeafdd" }, "sender": { "iban": "DE44100100100005571149", "paymentName": "Mario Liebeafdk", "accountHolder": "Mario Liebeafd" }, "bookingDate": 1480175147919 }}'
+  r = requests.post('https://hackathon.postbank.de:443/bank-api/gold/postbankid/credittransfer', headers=headers, data=data)
+  print json.loads(r.text)['links'][0]['href']
 
