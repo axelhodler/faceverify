83e4b3fe285767c72cd75d284e4fcd12a86edcee
Axel Hodler <derxor@gmail.com>
2016-11-26 19:06:55 +0100

    can finalize transfer

--- server/postbank.py
+++ server/postbank.py
@@ -17,14 +17,26 @@ def get_token():
   r = requests.post('https://hackathon.postbank.de/bank-api/gold/postbankid/token', headers=headers, data=data)
   return json.loads(r.text)['token']
 
-# Mario to Mariu
+def finalize_deposit_payment(finalization_url):
+  headers= {
+    "API-Key": API_KEY,
+    "x-auth": get_token(),
+    "Content-Type":"application/json"
+  }
+  r = requests.get(finalization_url, headers=headers)
+  print r.text
+
+# Mario (Attendee) to Mariu (Event Organizer)
 def deposit_payment():
   headers= {
     "API-Key": API_KEY,
     "x-auth": get_token(),
     "Content-Type":"application/json"
   }
-  data='{"authorizationDevice": {       "identifier": "668357","method": "bestSign","numberOfFailedAttempts": 0,"authorizationState": "SELECTED", "preferred": false, "aliasName": "HackathonNov01_8" }, "state": null, "creditTransfer": { "amount": 1, "recipient": { "iban": "DE18100100100005616140", "paymentName": "Mariu Freudeafdd", "accountHolder": "Mariu Freudeafdd" }, "sender": { "iban": "DE44100100100005571149", "paymentName": "Mario Liebeafdk", "accountHolder": "Mario Liebeafd" }, "bookingDate": 1480175147919 }}'
+  data='{"authorizationDevice": {"identifier": "668357","method": "bestSign","numberOfFailedAttempts": 0,"authorizationState": "SELECTED", "preferred": false, "aliasName": "HackathonNov01_8" }, "state": null, "creditTransfer": { "amount": 1, "recipient": { "iban": "DE18100100100005616140", "paymentName": "Mariu Freudeafdd", "accountHolder": "Mariu Freudeafdd" }, "sender": { "iban": "DE44100100100005571149", "paymentName": "Mario Liebeafdk", "accountHolder": "Mario Liebeafd" }, "bookingDate": 1480175147919 }}'
   r = requests.post('https://hackathon.postbank.de:443/bank-api/gold/postbankid/credittransfer', headers=headers, data=data)
-  print json.loads(r.text)['links'][0]['href']
+  finalizing_url = json.loads(r.text)['links'][0]['href']
+  finalize_deposit_payment(finalizing_url)
+
+
 
